2d2b9a926ddb2dcbd3834536909cac642c842f1d
Juliana <juliana.hachmann@gmail.com>
2026-02-14 19:24:38 -0300

    refactor: tira a dependência de FlagCards de page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,33 +1,11 @@
 'use client'
 
-import { useGameManager } from "@/src/hooks/useGameManager";
 import GameLayout from '@/src/components/GameLayout';
-import FlagCards from "@/src/components/FlagCards";
-import { useEffect, useState } from "react";
 
 export default function Home() {  
 
-    const { questions, game, checkAnswer, currentQuestion, resetGame, score, attempts } = useGameManager();
-
     return (  
-        <GameLayout 
-            title={"Trivia Game"} 
-            score={score} 
-            onReset={resetGame} 
-            game={game}
-            question={`País: ${currentQuestion?.getText()}`}
-        > 
-            { questions?.map((question, index) => (
-                <FlagCards
-                    key={question.getAnswer().getId()}
-                    flag={question.getAnswer()}
-                    index={Number(question.getAnswer().getId())}
-                    isHit={question.isHit()}
-                    attempts={question.getAttempts()}
-                    checkAnswer={checkAnswer}
-                />
-            ) ) }
-        </GameLayout>
+        <GameLayout />
     );
 
 };
